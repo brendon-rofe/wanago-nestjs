4e5e40279a1ff6b1bf304bd27a670d0ff5210cd2
Bravo1B9 <brofe93@gmail.com>
2023-05-17 13:46:56 +0000

    set nextPostId and posts

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from "@nestjs/common";
+import { Post } from "./post.interface";
 
 @Injectable()
 export class PostsService {
   
+  private nextPostId = 1;
+  private posts: Post[] = [];
+
 };
