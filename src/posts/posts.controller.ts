bc0fbb3887ba62525b51a951f370be5b5d91a31a
Bravo1B9 <brofe93@gmail.com>
2023-05-17 13:56:09 +0000

    Initial create post functionality in controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,6 +1,15 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { CreatePostDto } from "./dto/createPost.dto";
+import { PostsService } from "./posts.service";
 
-@Controller()
+@Controller('posts')
 export class PostsController {
   
+  constructor(private postsService: PostsService) {}
+
+  @Post()
+  createPost(@Body() dto: CreatePostDto) {
+    return this.postsService.createPost(dto);
+  };
+
 };
