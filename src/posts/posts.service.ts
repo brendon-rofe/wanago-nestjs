7e6bc11d835e84a80170c39ae3f2dafa96164d13
Bravo1B9 <brofe93@gmail.com>
2023-05-17 13:50:52 +0000

    Initial create post functionality in service

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from "@nestjs/common";
 import { Post } from "./post.interface";
+import { CreatePostDto } from "./dto/createPost.dto";
 
 @Injectable()
 export class PostsService {
   
   private nextPostId = 1;
   private posts: Post[] = [];
+
+  createPost(dto: CreatePostDto) {
+    const newPost: Post = {
+      id: this.nextPostId,
+      title: dto.title,
+      content: dto.content
+    };
+    this.posts.push(newPost);
+    return { message: 'New post created', post: newPost };
+  };
 
 };
