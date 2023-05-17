32c5ac43d1a7ea5953ba0b741efa6709cbf64d24
Bravo1B9 <brofe93@gmail.com>
2023-05-17 12:01:43 +0000

    Initial posts controller setup

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { PostsService } from "./posts.service";
+import { PostsController } from "./posts.controller";
 
 @Module({
   imports: [],
-  controllers: [],
+  controllers: [PostsController],
   providers: [PostsService],
 })
 
