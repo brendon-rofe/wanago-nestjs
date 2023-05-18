d00096dfd50f01c461c52a75f9c6dc796ba06c11
Bravo1B9 <brofe93@gmail.com>
2023-05-18 11:19:58 +0000

    Implement create user functionality

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,14 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { CreateUserDto } from "./dto/createUser.dto";
+import { UsersService } from "./users.service";
 
-@Controller()
+@Controller('users')
 export class UsersController {
-  
+  constructor(private usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() userData: CreateUserDto) {
+    return await this.usersService.create(userData);
+  };
+
 };
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,17 @@
 import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "./user.entity";
+import { Repository } from "typeorm";
+import { CreateUserDto } from "./dto/createUser.dto";
 
 @Injectable()
 export class UsersService {
-  
+  constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
+
+  async create(userData: CreateUserDto) {
+    const newUser = await this.userRepository.create(userData);
+    await this.userRepository.save(newUser);
+    return { message: 'New user created', user: newUser };
+  };
+
 };
