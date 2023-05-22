fd6c1621e1ff2f4285adcdbef08f35cbe6005c12
Bravo1B9 <brofe93@gmail.com>
2023-05-22 11:59:03 +0000

    Implement password hashing with bcrypt

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -3,14 +3,17 @@ import { UsersService } from "src/users/users.service";
 import { RegisterDto } from "./dto/register.dto";
 import PostgresErrorCode from "src/database/postgresErrorCode.enum";
 import { LoginDto } from "./dto/login.dto";
+import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class AuthenticationService {
   constructor(private userService: UsersService) {};
 
   async register(dto: RegisterDto) {
+    const salt = await bcrypt.genSalt();
+    const password = await bcrypt.hash(dto.password, salt);
     try {
-      return await this.userService.create(dto);
+      return await this.userService.create({ ...dto, password: password });
     } catch (error) {
       if(error?.code === PostgresErrorCode.UniqueViolation) {
         throw new HttpException("User with that email already exists", HttpStatus.BAD_REQUEST);
