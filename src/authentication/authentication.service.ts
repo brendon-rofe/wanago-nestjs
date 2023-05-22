65a34f2ffc8a531532bd71fcda3d20c7f2449a17
Bravo1B9 <brofe93@gmail.com>
2023-05-22 11:21:07 +0000

    Add unique email error handling

--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,13 +1,20 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { UsersService } from "src/users/users.service";
 import { RegisterDto } from "./dto/register.dto";
+import PostgresErrorCode from "src/database/postgresErrorCode.enum";
 
 @Injectable()
 export class AuthenticationService {
   constructor(private userService: UsersService) {};
 
   async register(dto: RegisterDto) {
-    return await this.userService.create(dto);
+    try {
+      return await this.userService.create(dto);
+    } catch (error) {
+      if(error?.code === PostgresErrorCode.UniqueViolation) {
+        throw new HttpException("User with that email already exists", HttpStatus.BAD_REQUEST);
+      };
+    };
   };
 
 };
