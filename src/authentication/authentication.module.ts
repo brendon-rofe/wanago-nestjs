d4fd3ec32153a9ac6e734af6f9ca8e671f6fb871
Bravo1B9 <brofe93@gmail.com>
2023-05-23 05:22:55 +0000

    update authentication module

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -2,11 +2,25 @@ import { Module } from "@nestjs/common";
 import { AuthenticationService } from "./authentication.service";
 import { AuthenticationController } from "./authentication.controller";
 import { UsersModule } from "src/users/users.module";
+import { JwtModule } from "@nestjs/jwt";
+import { PassportModule } from "@nestjs/passport";
+import { LocalStrategy } from "./local.strategy";
+import { JwtStrategy } from "./jwt.strategy";
 
 
 @Module({
-  imports: [UsersModule],
-  providers: [AuthenticationService],
+  imports: [UsersModule,
+    PassportModule, 
+    JwtModule.registerAsync({
+      useFactory: async() => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: {
+          expiresIn: `${process.env.JWT_EXPIRATION}s`,
+        },
+      })
+    })
+  ],
+  providers: [AuthenticationService, LocalStrategy, JwtStrategy],
   controllers: [AuthenticationController],
 })
 export class AuthenticationModule {};
