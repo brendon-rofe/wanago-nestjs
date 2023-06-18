f3583ed7f0c4efeecfd57a8d57e5c7afbf6cce5a
Bravo1B9 <brofe93@gmail.com>
2023-06-18 13:24:10 +0000

    Create create user functionality

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './user.entity';
+import { Repository } from 'typeorm';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 @Injectable()
-export class UsersService {};
+export class UsersService {
+  constructor(@InjectRepository(UserEntity) private userRepo: Repository<UserEntity>) {};
+
+  async create(dto: CreateUserDto) {
+    const newUser = this.userRepo.create(dto);
+    await this.userRepo.save(newUser);
+    return newUser
+  };
+};
