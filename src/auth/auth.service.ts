f9751d49d521491b5f1390220cf1dfe4d946251f
Bravo1B9 <brofe93@gmail.com>
2023-06-18 13:51:45 +0000

    Implement register functionality

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { RegisterDto } from './dtos/register.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {};
+export class AuthService {
+  constructor(private usersService: UsersService) {};
+
+  async register(dto: RegisterDto) {
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+    try{
+      const newUser = await this.usersService.create({
+        ...dto,
+        password: hashedPassword
+      });
+      newUser.hash = undefined;
+      return newUser;
+    } catch(error) {
+      if(error.code === '23505') {
+        throw new HttpException('User with that email already exists', HttpStatus.BAD_REQUEST);
+      };
+      throw new HttpException('Something went wrong', HttpStatus.INTERNAL_SERVER_ERROR);
+    };
+  };
+};
