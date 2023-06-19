773e8c1bcd537bd91f50f4fdb988c892489bdf2a
Bravo1B9 <brofe93@gmail.com>
2023-06-19 10:24:48 +0000

    Update

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, HttpCode, UseGuards, Req, Res, Get } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dtos/register.dto';
+import { LocalAuthenticationGuard } from './localAuth.guard';
+import JwtAuthenticationGuard from './jwtAuth.guard';
+import RequestWithUser from './requestWithUser.interface';
 
 @Controller('auth')
 export class AuthController {
@@ -10,4 +14,30 @@ export class AuthController {
   async register(@Body() dto: RegisterDto) {
     return this.authService.register(dto);
   };
+
+  @HttpCode(200)
+  @UseGuards(LocalAuthenticationGuard)
+  @Post('log-in')
+  async logIn(@Req() request: RequestWithUser, @Res() response: Response) {
+    const {user} = request;
+    const cookie = this.authService.getCookieWithJwtToken(user.id);
+    response.setHeader('Set-Cookie', cookie);
+    user.hash = undefined;
+    return response.send(user);
+  }
+
+  @UseGuards(JwtAuthenticationGuard)
+  @Post('log-out')
+  async logOut(@Res() response: Response) {
+    response.setHeader('Set-Cookie', this.authService.getCookieForLogOut());
+    return response.sendStatus(200);
+  }
+
+  @UseGuards(JwtAuthenticationGuard)
+  @Get()
+  authenticate(@Req() request: RequestWithUser) {
+    const user = request.user;
+    user.hash = undefined;
+    return user;
+  }
 };
